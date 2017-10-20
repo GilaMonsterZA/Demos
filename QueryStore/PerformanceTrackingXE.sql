3e6fa9b3550c68a753fc2164c8c3a40a11c4695f
Gail <gail@sqlinthewild.co.za>
2017-10-20 16:05:22 +0200

    Fix file path and file name

--- QueryStore/PerformanceTrackingXE.sql
+++ QueryStore/PerformanceTrackingXE.sql
@@ -5,7 +5,7 @@ ADD EVENT sqlserver.sp_statement_completed(
 ADD EVENT sqlserver.sql_statement_completed(SET collect_statement=(1)
     ACTION(sqlserver.database_id,sqlserver.query_hash)
     WHERE ([package0].[greater_than_int64]([duration],(100))))
-ADD TARGET package0.event_file(SET filename=N'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup\Temp.xel')
+ADD TARGET package0.event_file(SET filename=N'D:\Databases\Backup\PerformanceStats.xel')
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
 GO
 
